c795e009e913c318652249ae4f1b1b81477e9c24
xxyychen <853173351@qq.com>
2025-07-09 00:19:06 +0800

    王麻子v1.0

---
+++ 面试题/Q1.py
@@ -0,0 +1,16 @@
+# -*- coding = utf-8 -*-
+# @Time : 2025/7/1 16:27
+# @Author : 鬼鬼
+# @File : Q1.py
+# @Software: PyCharm
+
+def main():
+
+    s1 = "#123#123#"
+
+    print(s1.strip("#"))
+    print(s1.split("#"))
+
+
+if __name__ == "__main__":
+    main()
